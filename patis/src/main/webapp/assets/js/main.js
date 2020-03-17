186ffeb518d944bd655dc768efc68308a1e666a5
njm1128 <njm1128@naver.com>
2020-03-17 19:34:19 +0900

    메인 배너 슬라이드 동작 처리 완료

--- patis/src/main/webapp/assets/js/main.js
+++ patis/src/main/webapp/assets/js/main.js
@@ -1,9 +1,11 @@
 $(document).ready(function() {
   var currentSlide = 1;
-  var totalSlide = $("#slide-btn-js .slide-button").length;
+  var totalSlide = $("#slide-image-js li").length;
   var slideInterval;
   var slideSize;
-
+  var slideMove = 1;
+  var isPlay = 0;
+  
   if (totalSlide > 0) slidePlay();
 
   $("#slide-btn-js .slide-control").on("click", function() {
@@ -18,33 +20,57 @@ $(document).ready(function() {
 
   $("#slide-btn-js .slide-button").on("click", function() {
     var idx = $(this).index();
-    slideMove(idx);
+    
+    slideMove = idx == 0 ? -1 : 1;
+    if(isPlay == 0) slidePlay();
   });
 
   function slidePlay() {
     slideInterval = setInterval(function() {
-      slideNext();
+    	slideMove == -1 ? slidePrev() : slideNext(); 
     }, 3000);
     $("#slide-btn-js .slide-control button").removeClass("play");
     $("#slide-btn-js .slide-control button").addClass("pause");
+    isPlay = 1;
   }
 
   function slidePause() {
     clearInterval(slideInterval);
     $("#slide-btn-js .slide-control button").removeClass("pause");
     $("#slide-btn-js .slide-control button").addClass("play");
+    isPlay = 0;
   }
 
   function slideNext() {
+    clearInterval(slideInterval);
     if (currentSlide > totalSlide) currentSlide = 1;
 
+    currentSlide++;
+    
     slideSize = $("#slide-image-js li a")
       .css("width")
       .replace("px", "");
     var marginLeft = "-" + slideSize * (currentSlide - 1);
     $("#slide-image-js").animate({ "margin-left": marginLeft }, 500);
 
-    currentSlide++;
+    slidePlay();
+  }
+  
+  function slidePrev() {
+    clearInterval(slideInterval);
+    
+    if (currentSlide < 1) currentSlide = totalSlide;
+
+    currentSlide--;
+    
+    slideSize = $("#slide-image-js li a")
+      .css("width")
+      .replace("px", "");
+    var marginLeft = "-" + slideSize * (currentSlide - 1);
+    
+    $("#slide-image-js").animate({"margin-left": marginLeft }, 500);
+    
+    slidePlay();
   }
 
   function slideMove(idx) {
