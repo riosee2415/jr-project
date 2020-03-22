4668d7c810ff6a99eefae6bd5c0d531f445460de
njm1128 <njm1128@naver.com>
2020-03-22 23:16:00 +0900

    SNS 공유 페이스북 개발

--- patis/src/main/webapp/assets/js/sns_share.js
+++ patis/src/main/webapp/assets/js/sns_share.js
@@ -1,7 +1,10 @@
 $(document).ready(function() {
 	var SNS = {
 		naver: function(url, title) {
-			window.open("http://share.naver.com/web/shareView.nhn?url=" + encodeURI(encodeURIComponent("http://www.jungrangdosi.cafe24.com")) + "&title=" + encodeURI(title), "naver", "width=500, height=500");
+			window.open("http://share.naver.com/web/shareView.nhn?url=" + encodeURI(encodeURIComponent(url)) + "&title=" + encodeURI(title), "naver", "width=500, height=500");
+		},
+		facebook: function(url, title) {
+			window.open("http://www.facebook.com/share.php?u=" + encodeURIComponent(url) + "&t=" + encodeURIComponent(title), "facebook", "width=400, height=400")
 		},
 		twitter: function(url, text) {
 			window.open("http://twitter.com/share?url=" + encodeURIComponent(url) + "&text=" + encodeURIComponent(text), "twitter", "width=626, height=436");
@@ -21,9 +24,9 @@ $(document).ready(function() {
 		var sns = $(this).data('sns');
 		var url = window.location.href;
 		if(sns == 'naver') {
-			SNS.naver(url, '공유 테스트');
+			SNS.naver(url, '');
 		} else if(sns == 'facebook') {
-			
+			SNS.facebook(url, '')
 		} else if(sns == 'twitter') {
 			SNS.twitter(url, '');
 		}
--- patis/src/main/webapp/index.jsp
+++ patis/src/main/webapp/index.jsp
@@ -1,7 +1,12 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 pageEncoding="UTF-8"%>
 
+<%@ include file="/WEB-INF/views/template/components/common/html_header.jsp" %>
+
 <script>
   //location.href = "${pageContext.request.contextPath }/main.do";
   alert('서비스 준비중 입니다.');
 </script>
+
+<%@ include file="/WEB-INF/views/template/components/common/html_footer.jsp" %>
+
